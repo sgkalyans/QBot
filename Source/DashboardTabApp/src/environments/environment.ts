9cfa32b261b70ac80977cca63312e3a15626b350
sgkalyans <sgkalyans@gmail.com>
2020-10-08 15:35:00 +0800

    Update environment.ts

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiBaseUrl: "https://MyTpQBot9.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://mytpqbotnew.azurewebsites.net/api/Request/",
     
     authConfig: {
         instance: "https://login.microsoftonline.com/",
